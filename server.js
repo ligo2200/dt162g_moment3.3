ff380672efd2edccbe2cdb9f7997fd2506b99f96
Linda Götenmark <ligo2200@student.miun.se>
2023-12-19 11:32:56 +0100

    grundstruktur inför skapandet av routes

---
+++ server.js
@@ -0,0 +1,30 @@
+// pull in dotenv library
+require('dotenv').config()
+
+// pull in express library
+const express = require('express')
+// expressfuncion put i variable app
+const app = express()
+// pull in mongoose library
+const mongoose = require('mongoose')
+
+//connecting mongoose to mongodb
+mongoose.connect(process.env.DATABASE_URL)
+//variable with mongoose connection
+const db = mongoose.connection
+
+// log errors
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+// allow use of json
+app.use(express.json())
+
+// route setup
+const coursesRouter = require('./routes/courses')
+// app use router
+app.use('/courses', coursesRouter)
+
+
+//expressfunction (in variable app) listens to port 3000
+app.listen(3000, () => console.log('Server Started'))
