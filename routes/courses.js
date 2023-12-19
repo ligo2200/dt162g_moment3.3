6a3e25fada2951148682296952f9b88713f33c9f
Linda Götenmark <ligo2200@student.miun.se>
2023-12-19 14:55:39 +0100

    skelett för routes, påbörjar modell för courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,4 +1,31 @@
+// pull in express library
 const express = require('express')
 const router = express.Router()
+
+// get courses
+router.get('/', (req, res) => {
+    res.send('Hello wolrd!')
+})
+
+// get course by id
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+
+})
+
+// create course
+router.post('/', (req, res) => {
+
+})
+
+// update course
+router.patch('/', (req, res) => {
+
+})
+
+// deleting course
+router.delete('/:id', (req, res) => {
+
+})
 
 module.exports = router
